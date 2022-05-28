caaac12252c78b443524f9b190445c4118698041
SaraAzmin <azmin.sara17@gmail.com>
2022-05-28 12:43:06 +0200

    minor changes

--- index.js
+++ index.js
@@ -64,6 +64,20 @@ async function run() {
 
         }
 
+        //add a payment intent
+        app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+            const order = req.body;
+            const price = order.orderPrice;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({ clientSecret: paymentIntent.client_secret })
+        });
+
+
         //all instruments loaded
         app.get('/instruments', async (req, res) => {
             const query = {};
