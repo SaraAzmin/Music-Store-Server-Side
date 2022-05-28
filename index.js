4ac42450b1a69593fb8da78658e8e3f920e9a3e6
SaraAzmin <azmin.sara17@gmail.com>
2022-05-28 12:29:01 +0200

    minor changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 //midddlewere
 //app.use(cors());
